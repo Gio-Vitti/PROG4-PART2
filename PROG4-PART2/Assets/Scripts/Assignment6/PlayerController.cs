0e874d64fd86583895c1388ed05e40af6d628b85
Giovani Vitti <giovani.mamprin@icloud.com>
2026-04-13 12:30:38 -0400

    More progress on the character movement and the car enemy

--- PROG4-PART2/Assets/Scripts/Assignment6/PlayerController.cs
+++ PROG4-PART2/Assets/Scripts/Assignment6/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -5,7 +6,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
+    public float moveSpeed;
+    public float turnSpeed;
+
+    private bool lastClickedLeft = true;
+    public Transform body;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,22 +22,41 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
        //Move forward when mouse clicked
-       if(Mouse.current.leftButton.isPressed)
+       if(Mouse.current.leftButton.wasPressedThisFrame && !lastClickedLeft)
         {
-            transform.position += transform.forward * speed;
+            transform.position += transform.forward * moveSpeed;
+            lastClickedLeft = true;
+        }
+
+        if (Mouse.current.rightButton.wasPressedThisFrame && lastClickedLeft)
+        {
+            transform.position += transform.forward * moveSpeed;
+            lastClickedLeft = false;
         }
 
         //Rotate with scroll wheel
-        Vector2 scroll = Mouse.current.scroll.readValue();
+        Vector2 scroll = Mouse.current.scroll.ReadValue();
 
         if (scroll.y > 0)
         {
-            transform.Rotate(Vector3.up * speed);
+            transform.Rotate(Vector3.up * turnSpeed);
         }
 
         if (scroll.y < 0)
         {
-            transform.Rotate(Vector3.up * speed);
+            transform.Rotate(Vector3.up * -turnSpeed);
+        }
+
+        //Simple character animations
+        if (lastClickedLeft)
+        {
+            body.eulerAngles = new Vector3(0, transform.eulerAngles.y, 30);
+        }
+
+        //Simple character animations
+        if (!lastClickedLeft)
+        {
+            body.eulerAngles = new Vector3(0, transform.eulerAngles.y, -30);
         }
     }
 }
